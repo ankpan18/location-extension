956dc99ff2144a8ffea8ceecab011cc1ba18e650
Ankur Panthri <panthriankur@outlook.com>
2023-09-03 12:06:30 +0530

    added demo video

--- src/features/location-button.tsx
+++ src/features/location-button.tsx
@@ -6,6 +6,7 @@ const Location = () => {
   const [data, setData] = useState("")
   const [loc, setLoc] = useState({})
   const [error, setError] = useState(false)
+  let timeoutId;
   const handleClick = () => {
     if(loading==false)
     {
@@ -24,7 +25,7 @@ const Location = () => {
     const jsonData = await fetchData()
     console.log("jsonData:",jsonData);
     const response = await fetch(
-      `https://ipinfo.io/${jsonData["ip"]}?token=706b32a27c061b`
+      `https://ipinfo.io/${jsonData["ip"]}?token=${process.env.PLASMO_PUBLIC_API_KEY}`
     )
     const locationData = await response.json()
     setLoc(locationData);
@@ -47,37 +48,51 @@ const Location = () => {
         }
         setLoading(false);
       })
+      // timeoutId = setTimeout(() => {
+      //   // If the connection is not successful after 10 seconds, set the error state to true
+      //   if (JSON.stringify(loc)==="{}") {
+      //     setError(true);
+      //     loc["error"]["title"]="Internet Error";
+      //     loc["error"]["message"]="Unable to connect to the server";
+      //     setLoading(false);
+      //   }
+      // }, 10000);
     } else {
       //setLoading(false);
       setDesc("Show my Location")
     }
+    // Clear the timeout when the component unmounts
+    // return () => {
+    //   clearTimeout(timeoutId);
+    // };
   }, [loading])
 
   return (
-    <>
-      <div className="bg-yellow-200 text-lg">
-        {" "}
-        {loading || JSON.stringify(loc)==="{}"? (
-          <></>
-        ) : (
-          error? (<h2>Error: {loc["error"]["title"]} {loc["error"]["message"]} </h2>): (
-            <h2>
-              My Country name is {loc["country"]} and city is {loc["city"]}{" "}
-              {JSON.stringify(loc)}{" "}
-            </h2>
-          )
-        )}
+      <div> 
+          
+        <div className="bg-yellow-200 text-lg absolute mt-24 w-full text-center font-semibold">
+          
+          {loading || JSON.stringify(loc)==="{}" ? (
+            <></>
+          ) : (
+            error? (<h2>Error: {loc["error"]["title"]} {loc["error"]["message"]} </h2>): (
+              <h2>
+                My Country name is {loc["country"]} and city is {loc["city"]}
+              </h2>
+            )
+          )}
+          
+        </div>
         
+        <div className="h-screen bg-yellow-200 w-full flex justify-center items-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={handleClick}>
+            {desc}
+          </button>
+        </div>
+
       </div>
-      <div className="h-screen bg-yellow-200 w-full flex justify-center items-center">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleClick}>
-          {desc}
-        </button>
-      </div>
-      <div />
-    </>
   )
 }
 
